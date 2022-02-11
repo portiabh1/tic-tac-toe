41a9a8096f154baa769ddf0e1c924e26d7231618
vedajammula <41130279+vedajammula@users.noreply.github.com>
2022-02-11 12:21:09 -0700

    Created PlaceMarker Function
    Compared the chosen location from user to the location on the board and placed a marker if the locations are the same

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -22,3 +22,16 @@ void DisplayBoard(string Board[3][3]){
   }
 }
 
+string PlaceMarker(String[][] location, bool marker)
+{
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      if (location == Board[i][j]){
+        marker == true;
+      }
+      else{
+        marker == false;
+      }
+    }
+  }
+}
